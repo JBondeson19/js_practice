4008af8d7915d751a56322ec591a376a375364c7
James Bondeson <jpbond58@gmail.com>
2021-03-31 01:33:22 -0400

    executing functions

--- basics-07-function-with-return/assets/scripts/app.js
+++ basics-07-function-with-return/assets/scripts/app.js
@@ -1,14 +1,17 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
-}
 
- 
+
+// 'add'function below 
 currentResult = add(1, 2);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
+
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
