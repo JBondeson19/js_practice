fae308ba46273941898d8409c1da8ec83d9e16be
James Bondeson <jpbond58@gmail.com>
2021-03-30 23:26:33 -0400

    playing w funcitons

--- basics-04-added-constant/assets/scripts/app.js
+++ basics-04-added-constant/assets/scripts/app.js
@@ -1,13 +1,18 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${defaultResult}  + 10) * 3 / 2 - 1`;
 
-let errorMessage =  'Oops! \n' + 
-                    'It broke.'
-
-
 
 outputResult(currentResult, calculationDescription);
+
+function addNumbers(num1, num2) {
+    const result = num1 + num2;
+    alert(`The result is ${result}!`)
+}
+
+addNumbers(1, 2);
---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,13 @@
+let firstVar = 4
+
+let newResult;
+
+newResult = firstVar + 18
+
+newResult = (firstVar - 5) * 3 / 2
+
+alert(firstVar)
+
+alert(newResult)
+
+
