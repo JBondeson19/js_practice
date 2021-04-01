db2f5a0bf810ee0880f51f49b0807064716a0b64
James Bondeson <jpbond58@gmail.com>
2021-04-01 19:22:30 -0400

    "second assignment"

--- basics-assignment-2-start/assignment.js
+++ basics-assignment-2-start/assignment.js
@@ -1,1 +1,16 @@
 const task3Element = document.getElementById('task-3');
+
+
+function Hey  (){
+        return alert("I'm cool")
+    }
+    
+    Hey()
+    
+    function ItsMe(name){
+        alert(name)
+    }
+    
+    ItsMe("James")
+
+    task3Element.addEventListener('click', Hey)
