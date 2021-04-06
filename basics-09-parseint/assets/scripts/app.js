f2a07651bd691306280b091d4ad7dcc7b4848e11
James Bondeson <jpbond58@gmail.com>
2021-04-06 16:21:44 -0400

    built buttons and refactored

--- basics-09-parseint/assets/scripts/app.js
+++ basics-09-parseint/assets/scripts/app.js
@@ -2,15 +2,55 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function getUserInputNumber () {
-  return +userInput.value
+  return +userInput.value;
 }
 // added this function to extract info from userInput and keep code DRY
 
-function add() {
-  const enteredNumber = getUserInputNumber();
-  const calcDescription = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
+function createAndWriteOutput(operator, resultBeforeCal, calcNumber) {
+  const calcDescription = `${resultBeforeCal} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
 }
 
+// DRYing out 'outPutResult' at the end of each function
+
+function add() {
+  const enteredNumber = getUserInputNumber();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+}
+// refactored this to make variables DRY
+
+function subtract () {
+  const enteredNumber = getUserInputNumber();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+
+// new function - copied add() and refactored
+
+function multiply () {
+  const enteredNumber = getUserInputNumber();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+// new function 
+
+function divide () {
+  const enteredNumber = getUserInputNumber();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
+// new function
+
+
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
