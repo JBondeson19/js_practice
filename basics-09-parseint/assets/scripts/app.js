72e0cb7def6c748e24df01aa194bef3d2fe35fa4
James Bondeson <jpbond58@gmail.com>
2021-04-06 15:42:40 -0400

    dry code

--- basics-09-parseint/assets/scripts/app.js
+++ basics-09-parseint/assets/scripts/app.js
@@ -1,9 +1,16 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserInputNumber () {
+  return +userInput.value
+}
+// added this function to extract info from userInput and keep code DRY
+
 function add() {
-  currentResult = currentResult + +userInput.value;
-  outputResult(currentResult, '');
+  const enteredNumber = getUserInputNumber();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
