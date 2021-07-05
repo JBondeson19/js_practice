01fa8531ae6a49aa1c25fb6afdce06bb10dcb09a
James Bondeson <jpbond58@gmail.com>
2021-07-05 15:15:53 -0400

    "success"

--- sec4/control-assignment-problem/assignment.js
+++ sec4/control-assignment-problem/assignment.js
@@ -1,1 +1,36 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+const newRandomNumber = Math.random();
+
+function tooBig(randomNumber){
+    if (randomNumber > 0.7){
+        console.log(randomNumber);
+        alert("This number is too big!!!")
+    } else {
+        console.log(randomNumber);
+    }
+}
+
+
+function displayNumbers(){
+    numbers = [5,19,23,17,14,26,7, 18, 92, 103]
+    
+    for (i = numbers.length -1; i >=0 ;i-- ){
+        console.log(numbers[i]);
+    }
+
+    for (const index of numbers){
+        console.log(index);
+    }
+}
+
+function conditionalAlert(randomNumber, newRandomNumber){
+    console.log(randomNumber, newRandomNumber);
+
+    if (randomNumber && newRandomNumber > 0.7){
+       alert("It's too big!");
+    } else if (randomNumber || newRandomNumber < 0.2){
+        alert("It's not big enough!");
+    }
+}
+
+conditionalAlert(randomNumber,newRandomNumber);
