17d2d1045dc12d1fbf973a536e61b498d01406b5
James Bondeson <jpbond58@gmail.com>
2021-07-13 20:17:17 -0400

    passed

--- sec6/functions-assignment-problem/assignment.js
+++ sec6/functions-assignment-problem/assignment.js
@@ -1,5 +1,11 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
-}
+const sayHello = name  => console.log('Hi ' + name);
 
-sayHello();
+const sayHello2 = (name, phrase)  => console.log(phrase + ' ' + name);
+const sayHello3 = ()  => console.log('Hi James' );
+const sayHello4 = name  => 'Hi ' + name;
+
+
+sayHello('James');
+sayHello2("James", "Sup ");
+sayHello3();
+console.log(sayHello4("James"));
